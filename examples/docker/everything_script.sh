4774764ada4320c4d0eda2bf21ef44553b6f1128
nunomaga <nunomagalhaes.7046@gmail.com>
2021-03-12 19:00:21 +0000

    update

--- examples/docker/everything_script.sh
+++ examples/docker/everything_script.sh
@@ -4,7 +4,7 @@ pip3 install mlflow #kinda self-nexplanatory
 pip3 install kubernetes #mlflow run --backend kubernetes requirement
 docker run -p 5000:5000 registry & #can be replaced with gcloud registry
 git clone https://github.com/NunoMaga/mlflow #personal fork
-
+cd mlflow/examples/docker
 docker build -t localhost:5000/docker-example-base -f Dockerfile . #base image dependency build, we need to update the image to have our conda env
 docker push localhost:5000/docker-example-base #push to localhost. very much unnecessary
 #mlflow run . -P alpha=0.5  //docker run example
