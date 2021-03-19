9c751548c00c2cfce111400a296012782e2934fe
nunomaga <nunomagalhaes.7046@gmail.com>
2021-03-19 18:24:25 +0000

    magikarp tested, working!

--- examples/docker/everything_script.sh
+++ examples/docker/everything_script.sh
@@ -1,11 +1,29 @@
 #!/bin/sh
+
+# dependencies
 sudo apt install python3-pip
-pip3 install mlflow #kinda self-nexplanatory
-pip3 install kubernetes #mlflow run --backend kubernetes requirement
-docker run -p 5000:5000 registry & #can be replaced with gcloud registry
-git clone https://github.com/NunoMaga/mlflow #personal fork
-cd mlflow/examples/docker
-docker build -t localhost:5000/docker-example-base -f Dockerfile . #base image dependency build, we need to update the image to have our conda env
-docker push localhost:5000/docker-example-base #push to localhost. very much unnecessary
-#mlflow run . -P alpha=0.5  //docker run example
-mlflow run . --backend kubernetes --backend-config kubernetes_config.json -P alpha=0.5
+pip install mlflow 
+pip install kubernetes 
+
+#install gcloud §from https://cloud.google.com/storage/docs/gsutil_install
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+sudo apt-get install apt-transport-https ca-certificates gnupg
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+sudo apt-get update && sudo apt-get install google-cloud-sdk
+gcloud init
+
+#add named gke_hv-gcp-trycatalog_us-central1-c_magikarp-cluster [MAGIKARP] context 
+gcloud container clusters get-credentials \
+magikarp-cluster \
+--zone us-central1-c \
+--project hv-gcp-trycatalog
+
+# build base image
+docker build -t     us.gcr.io/hv-gcp-trycatalog/mlaas/AutoDEPPA2/AutoDEPPA2-base -f Dockerfile .
+
+# push to google registry
+docker push         us.gcr.io/hv-gcp-trycatalog/mlaas/AutoDEPPA2/AutoDEPPA2-base
+
+# build & push mlflow project image
+# create & run job
+mlflow run .        --backend kubernetes --backend-config kubernetes_config.json -P alpha=0.5
